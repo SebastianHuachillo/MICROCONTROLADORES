4d9b4243ec4ffc00f383577700957e50ff22b766
Semahuca <semahuca45@hotmail.com>
2020-08-20 11:05:33 -0500

    Clases

---
+++ ASM/Hola.X/build/default/production/Principal.o.d
@@ -0,0 +1,3 @@
+build/default/production/Principal.o:  \
+	C:/Users/sebastian/Desktop/MPLAB/ASM/Hola.X/Principal.asm  \
+
---
+++ ASM/OR.X/build/default/production/maincode.o.d
@@ -0,0 +1,3 @@
+build/default/production/maincode.o:  \
+	C:/Users/sebastian/Desktop/MPLAB/ASM/OR.X/maincode.asm  \
+
